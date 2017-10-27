5ebe586e4feca805a7bd484fc54b95c42395bfa0
Gyulai László <gyulas@sztaki.hu>
2017-10-27 23:33:25 +0200

    skeleton created

---
+++ interruptBased/interruptBased.ino
@@ -0,0 +1,49 @@
+#include "main.h"
+
+void timingISR(void)
+{
+	iISR++;
+	uled=true;
+		
+	if(!(iISR%2))
+	{
+		uadc=true;
+	}
+	
+	if(!(iISR%8))
+	{
+		usound=true;
+	}
+	
+}
+
+
+
+void main() {
+  Timer1.initialize(50000);
+  delay(100);
+  Timer1.attachInterrupt(timingISR);
+
+  while(1)
+  {
+    //handle leds
+	if(uled)
+	{
+		//TODO: write led refresh
+	}
+	
+	//handle adc
+	if(uadc)
+	{
+		//TODO: write adc read
+	}
+	
+	//handle sound
+	if(usound)
+	{
+		//TODO: write sound fade
+	}
+	
+	//handle button pushes? with adc.
+  }
+}
---
+++ interruptBased/main.h
@@ -0,0 +1,13 @@
+bool uled=false;
+bool uadc=false;
+bool usound=false;
+
+byte iISR=0;
+
+byte finR=0;
+byte finG=0;
+byte finB=0;
+
+byte curR=0;
+byte curB=0;
+byte curG=0;

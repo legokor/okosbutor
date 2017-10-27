90391e5e8ecca325b95543c32609a9359f5c8278
Gyulai László <gyulas@sztaki.hu>
2017-10-28 01:15:39 +0200

    elvetettuk a sok c sok header otletet, egzesitve

--- interruptBased/interruptBased.ino
+++ interruptBased/interruptBased.ino
@@ -1,7 +1,5 @@
 #include "main.h"
-#include "adc.c"
-#include "led.c"
-#include "sound.c"
+
 
 void timingISR(void)
 {
@@ -17,18 +15,88 @@ void timingISR(void)
 	{
 		usound=true;
 	}
-	
 }
 
+void ADCread()
+//
+//		2017.10.27. 23:58: ready to be tested.
+//
+//		fcn-s copied from program before.
+//		changelog: using map function.
+//
+//  INPUT variables:
+//      - three Analog channel
+//      - button pin
+//
+//  OUTPUT variables:
+//      - bool sensorState
+//      - bool nextSound
+//      - max_adc
+{
+//////////////////////////////BUTTON HANDLING/////////////////////////////////////////
+  button=digitalRead(buttonPin);
+  if(prevButton<button) 
+      {
+        buttonTimer=millis();                     // rise edge start buttonTimer
+      }
+  if(prevButton>button && ((millis()-buttonTimer)<2500))
+      {
+        nextSound=true;                        // @rise_edge if pos_level shorter than 2sec
+      }
+  else if(prevButton>button){                                         // @fall_edge if pos_level longer than 2sec
+        colorPalette = 0; //((1==colorPalette) ? (0) : (colorPalette+1));  // predefined color sets
+      }
+  prevButton=button;
 
 
-void main() {
+  /////////////////////////////////////////////////////////////////////////////////////
+  //AVERAGING 4 values, using map fcn
+  int lastread = 0;
+  lastread=(3==lastread)?(0):(lastread+1);
+  
+  t_s1_adc[lastread] = analogRead(distPin1);
+  t_s2_adc[lastread] = analogRead(distPin2);
+  t_s3_long_adc[lastread] = analogRead(distLong); //special sensor
+  s1_adc=0;
+  s2_adc=0;
+  s3_long_adc=0;
+  
+  int avg_i;
+  for(avg_i=0;avg_i<4;avg_i++)
+  {
+    s1_adc+=map(t_s2_adc,0,1023,0,63);
+    s2_adc+=map(t_s2_adc,0,1023,0,63);
+    s3_long_adc+=map(t_s2_adc,0,1023,0,127); //different mapping, since high voltage means bigger distance
+  }
+	
+  //MAXIMUM calculation, sensorstate
+  max_adc=max(s1_adc,max(s2_adc,s3_long_adc));
+  prevsensorstate=sensorstate;
+  sensorstate= (sense_radius < max_adc)?(true):(false); //true if people there
+
+ ///////////////////////FOR DEBUG AND INFORMATION//////////////////////////// 
+  
+  if(0==lastread)
+           {
+              /*Serial.print(s1_adc);
+              //Serial.print("\t");
+              Serial.print(s2_adc);
+              //Serial.print("\t");
+              Serial.println(s3_long_adc); */
+           }
+}//END OF ADCread
+
+
+
+void setup()
+{
   Timer1.initialize(50000);
   delay(100);
   Timer1.attachInterrupt(timingISR);
+}
 
-  while(1)
-  {
+void loop()
+{
     //handle leds
 	if(uled)
 	{
@@ -48,5 +116,5 @@ void main() {
 	}
 	
 	//handle button pushes? with adc.
-  }
+  
 }
--- interruptBased/main.h
+++ interruptBased/main.h
@@ -1,3 +1,9 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+#include "TimerOne.h"
+
+
 bool uled=false;
 bool uadc=false;
 bool usound=false;
@@ -5,3 +11,48 @@ bool usound=false;
 byte iISR=0;
 
 
+///ADC OUTPUTS
+bool sensorstate = 0;
+bool nextSound=false;
+volatile word max_adc;
+int colorPalette=0;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*                      ADC FUNCTION HEADER                      */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////INPUT VARIABLES//////
+#define distLong 5
+#define distPin1 6
+#define distPin2 7
+#define buttonPin 12
+
+/////////CONSTANTS///////////
+const byte sense_radius = 40;
+
+
+//////////ITERATORS/////////
+int lastread = 0;
+int avg_i = 0;
+
+/////INTERNAL VARIABLES/////
+
+volatile word s1_adc = 0;
+volatile word s2_adc = 0;
+volatile word s3_long_adc = 0;
+volatile word t_s1_adc[8] = { 0,0,0,0,0,0,0,0 };
+volatile word t_s2_adc[8] = { 0,0,0,0,0,0,0,0 };
+volatile word t_s3_long_adc[8] = { 0,0,0,0 };
+
+
+///////OUTPUT VARIABLES/////
+bool prevsensorstate = 0;
+bool button=0;
+bool prevButton=0;
+
+//for color/music changes
+int buttonTimer = 0;
+////////////////////////////////////////////////////////////////////////////////////////////

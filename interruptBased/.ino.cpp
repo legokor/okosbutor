abebb4f039f2ca465696449e440011393a964c44
Gyulai László <gyulas@sztaki.hu>
2017-11-22 14:37:37 +0100

    sound play - timeout define

--- interruptBased/.ino.cpp
+++ interruptBased/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-11-17 02:36:31
+//This file has been generated on 2017-11-22 14:35:46
 
 #include "Arduino.h"
 #include "main.h"
